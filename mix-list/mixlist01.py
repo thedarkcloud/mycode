309ebafda56658e8e56e0acccb37cb8f53e3be4f
David K <student@bchd.83725609-73a0-419e-bc51-b0667083c05c>
2021-07-19 18:47:19 +0000

    about complex lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# create a list containing 3 items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# display first item in the list
+print("The first item in the list (IP): " + my_list[0] )
+
+# display second item in the list
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# display the 3rd item in the list
+print("The last item in the list (state): " + my_list[2] )
+
